bf227988bbf3192a392dd63728d25b719d18ec59
Patrick Lehmann <Paebbels@gmail.com>
2026-02-16 15:06:49 +0100

    Updated regression description.

--- regression.tcl
+++ regression.tcl
@@ -1,7 +1,39 @@
+# =============================================================================
+# Authors:
+#   Patrick Lehmann
+#   Adrian Weiland
+#
+# License:
+# =============================================================================
+# Copyright 2025-2026 The BigDesign Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
 namespace eval ::poc {
-	variable myConfigFile  "../../../src/PoC/my_config.vhdl"
-	variable myProjectFile "../../../src/PoC/my_project.vhdl"
-	variable vendor "GENERIC"; # GENERIC for vendor-less build; Xilinx, Altera,... for vendor specific build
+	proc getEnv {var {default ""}} {
+		if {[info exists ::env($var)]} {
+			return $::env($var)
+		}
+		return $default
+	}
+
+	variable vendorName [getEnv VENDOR "GENERIC"]
+	variable boardName  [getEnv BOARD  "GENERIC"]
+
+	variable myConfigFile  "../tb/common/my_config_$boardName.vhdl"
+	variable myProjectFile "../tb/common/my_project.vhdl"
+
+	variable vendor $vendorName; # GENERIC for vendor-less build; Xilinx, Altera,... for vendor specific build
 }
 
 source ../lib/OSVVM-Scripts/StartUp.tcl
@@ -11,7 +43,7 @@ build ../lib/OsvvmLibraries.pro
 
 if {$::osvvm::ToolName eq "GHDL"} {
 	SetExtendedAnalyzeOptions {-frelaxed -Wno-specs -Wno-elaboration}
-    SetExtendedSimulateOptions {-frelaxed -Wno-specs -Wno-binding}
+	SetExtendedSimulateOptions {-frelaxed -Wno-specs -Wno-binding}
 
 	library unisim
 	analyze ../tb/unisim/vcomponents.pkg.vhdl
@@ -20,12 +52,9 @@ if {$::osvvm::ToolName eq "GHDL"} {
 	set RivieraSimOptions {-unbounderror}
 
 	LinkLibrary unisim {C:/Tools/precompiled/Riviera-PRO/2025.10/Vivado/2025.2/unisim}
-	# LinkLibrary axi_infrastructure_v1_1_0 {C:/Tools/precompiled/Riviera-PRO/2025.10/Vivado/2025.2/axi_infrastructure_v1_1_0}
-	# LinkLibrary axi_vip_v1_1_22 {C:/Tools/precompiled/Riviera-PRO/2025.10/Vivado/2025.2/axi_vip_v1_1_22}
-	# LinkLibrary zynq_ultra_ps_e_vip_v1_0_22 {C:/Tools/precompiled/Riviera-PRO/2025.10/Vivado/2025.2/zynq_ultra_ps_e_vip_v1_0_22}
 
 } elseif {$::osvvm::ToolName eq "NVC"} {
-   SetExtendedAnalyzeOptions {--relaxed}
+	SetExtendedAnalyzeOptions {--relaxed}
 
 	library unisim
 	analyze ../tb/unisim/vcomponents.pkg.vhdl
@@ -41,6 +70,8 @@ Supported simulators:
   - GHDL
   - RivieraPRO
   - NVC
+Other tools:
+  - Sigasi in VSCode
 ======================================
 } $::osvvm::ToolName]
 }
