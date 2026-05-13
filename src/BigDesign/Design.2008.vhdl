7cbacfcd2c7de5759e45c5aaeec1dbb902117218
adrian.weiland <adrian.weiland@plc2.de>
2026-05-13 18:46:38 +0200

    fix signals

--- src/BigDesign/Design.2008.vhdl
+++ src/BigDesign/Design.2008.vhdl
@@ -53,7 +53,7 @@ end entity;
 architecture rtl of Design is
 	-- Clocks
 	signal PS_Clock  : std_logic;
-	signal PL_Reset  : std_logic;
+	signal PL_Reset  : std_logic := '0';
 
 	signal Clock_50  : std_logic;
 	signal Clock_100 : std_logic;
@@ -99,12 +99,16 @@ begin
 		);
 
 	Demux_blk : block
-		--                                                           GPIO
-		constant BASE_ADDRESS      : T_SLUV               := (0 => 32x"0000");
-		constant BASE_ADDRESS_MASK : BASE_ADDRESS'subtype := (0 => 32x"E000");  -- 3 select bits
+		--                                                           GPIO            HRC
+		constant BASE_ADDRESS      : T_SLUV               := (0 => 40x"0000");--, 1 => 40x"2000");
+		constant BASE_ADDRESS_MASK : BASE_ADDRESS'subtype := (0 to 0 => 40x"E000");  -- 3 select bits
 
 		signal DeMux_Out_m2s : AXI4Lite_A40_D32.Sized_M2S_vector(BASE_ADDRESS'range);
 		signal DeMux_Out_s2m : AXI4Lite_A40_D32.Sized_S2M_vector(BASE_ADDRESS'range);
+
+		-- UART
+		signal UART_TX : std_logic;
+		signal UART_RX : std_logic := '1';
 
 	begin
 		AXI4L_DeMux: entity PoC.AXI4Lite_DeMux
@@ -127,9 +131,49 @@ begin
 
 			Config_GPIO_m2s  <= DeMux_Out_m2s(0);
 			DeMux_Out_s2m(0) <= Config_GPIO_s2m;
+
+		-- High resolution clock
+		--HRC: entity PoC.AXI4Lite_HighResolutionClock
+		--	generic map (
+		--		CLOCK_FREQUENCY      => 100 MHz,
+		--		USE_CDC              => True
+		--		--SECOND_RESOLUTION    => SECOND_RESOLUTION
+		--	)
+		--	port map (
+		--		Clock        => PS_Clock,
+		--		Reset        => PL_Reset,
+		--
+		--		AXI_clock    => PS_Clock,  -- todo: check
+		--		AXI_reset    => PL_Reset,  -- todo: check
+		--
+		--		AXI4Lite_m2s => DeMux_Out_m2s(1),
+		--		AXI4Lite_s2m => DeMux_Out_s2m(1),
+		--
+		--		Nanoseconds  => open,
+		--		Datetime     => open
+		--	);
+
+		-- UART
+		--UART: entity PoC.AXI4Lite_UART
+		--	generic map (
+		--		CLOCK_FREQ  => 100 MHz
+		--	)
+		--	port map (
+		--		Clock       => PS_Clock,
+		--		Reset	      => PL_Reset,
+		--
+		--		Config_m2s  => DeMux_Out_m2s(2),
+		--		Config_s2m  => DeMux_Out_s2m(2),
+		--		Config_IRQ  => open,
+		--
+		--		UART_TX	    => UART_TX,
+		--		UART_RX	    => UART_RX,
+		--		UART_RTS    => open,
+		--		UART_CTS    => 'U'
+		--	);
 	end block;
 
-	blkGPIO : block
+	GPIO_blk : block
 		constant CONFIG : T_AXI4_Register_Vector := (
 			to_AXI4_Register(Name => "Buttons", Address => 32x"00", RegisterMode => ReadOnly_NotRegistered),
 			to_AXI4_Register(Name => "LEDs",    Address => 32x"04", RegisterMode => ReadWrite)
