1b4117c56f3c7544fb12709a8dd24a251769062b
Patrick Lehmann <Paebbels@gmail.com>
2026-06-03 20:31:07 +0200

    Fixed sizes of user bits.
    (cherry picked from commit abfe681f25f3a646db31a44b7a5cedb1558bafc7)

--- regression.tcl
+++ regression.tcl
@@ -63,8 +63,8 @@ if {$::osvvm::ToolName eq "GHDL"} {
 	set RivieraSimOptions {-unbounderror}
 	set ::BigDesign::scalingFactor 1
 
-	LinkLibrary unisim {C:/Tools/precompiled/Riviera-PRO/2025.10/Vivado/2025.2/unisim}
-
+	# FIXME: this is a hardcoded path
+	LinkLibrary unisim {C:/Tools/precompiled/Riviera-PRO/2026.04/Vivado/2025.2/unisim}
 } elseif {$::osvvm::ToolName eq "NVC"} {
 	SetExtendedAnalyzeOptions {--relaxed}
 
